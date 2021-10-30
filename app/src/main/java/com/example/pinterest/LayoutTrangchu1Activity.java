32ac417b37e687fbbaa26f7755862b4b2fba4c0c
trungkien051000 <71768311+trungkien051000@users.noreply.github.com>
2021-10-30 15:53:55 +0700

    let do it

---
+++ app/src/main/java/com/example/pinterest/LayoutTrangchu1Activity.java
@@ -0,0 +1,40 @@
+package com.example.pinterest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LayoutTrangchu1Activity extends AppCompatActivity {
+    TextView name;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_trangchu_1);
+
+         name = (TextView) findViewById(R.id.txt_clicked);
+         image = findViewById(R.id.img_clicked);
+
+         Intent intent = getIntent();
+         name.setText(intent.getStringExtra("name"));
+         image.setImageResource(intent.getIntExtra("image",0));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
